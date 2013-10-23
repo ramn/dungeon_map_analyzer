5d90965e11dc8858975eb274f7db7fe77efafd18
ramn <github@rymdimperiet.org>
2013-10-24 00:24:56 +0200

    Generate graph from map

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -7,5 +7,32 @@ object Main extends App {
   val pathToYamlMap = Paths.get(args(0))
 
   val document = new YamlDocumentParser(pathToYamlMap)
-  document.print
+  //document.print
+
+  val graph = new MapGraph(document.map)
+}
+
+
+import scalax.collection.Graph
+import scalax.collection.edge.LDiEdge
+import scalax.collection.GraphPredef._
+import scalax.collection.GraphEdge._
+
+class MapGraph(roomsWithExits: Map[String, Map[String, String]]) {
+  protected val allPairs: Iterable[LDiEdge[String]] = roomsWithExits
+    .flatMap { case (sourceRoom, exits) =>
+      exits
+        .filter { case (exit, destRoom) =>
+          sourceRoom != null && destRoom != null && exit != null
+        }
+        .map { case (exit, destRoom) =>
+          LDiEdge(sourceRoom, destRoom)(exit)
+        }
+    }
+
+  val graph = allPairs.foldLeft(Graph.empty[String, LDiEdge])(_ + _)
+
+  println(graph)
+  println
+  println(graph.edges)
 }
