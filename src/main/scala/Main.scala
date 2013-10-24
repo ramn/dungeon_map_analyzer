1753384fefce980d20551da2b2ec8f33e2880ba7
ramn <github@rymdimperiet.org>
2013-10-24 08:44:48 +0200

    Accept yaml path as arg or from env

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,10 +4,9 @@ import java.nio.file.Paths
 
 
 object Main extends App {
-  val pathToYamlMap = Paths.get(args(0))
+  val pathToYamlFile = args.toList.headOption.getOrElse(sys.env("MAP_FILE"))
+  val pathToYamlMap = Paths.get(pathToYamlFile)
 
   val document = new YamlDocumentParser(pathToYamlMap)
-  //document.print
-
   val graph = new MapGraph(document.map)
 }
