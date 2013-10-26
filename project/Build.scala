1fc59aea9b07d38d510e386568bce82759922224
ramn <github@rymdimperiet.org>
2013-10-26 21:12:55 +0200

    Update to SBT 0.13.0
    Remove sbt start plugin, not compatible with sbt 0.13.0

--- project/Build.scala
+++ project/Build.scala
@@ -1,18 +1,14 @@
 import sbt._
 import sbt.Keys._
-import com.typesafe.sbt.SbtStartScript
 
 object Build extends sbt.Build {
   lazy val root = Project(
     id = "dungeon_map_analyzer",
     base = file("."),
-    settings = Defaults.defaultSettings
-      ++ mySettings
-      ++ SbtStartScript.startScriptForClassesSettings
+    settings = Defaults.defaultSettings ++ mySettings
   )
 
   lazy val mySettings = Seq(
-    mainClass in Compile := None, // forces us to use explicit class with target/start
     organization := "se.ramn",
     version := "0.1-SNAPSHOT",
     scalaVersion := "2.10.3",
